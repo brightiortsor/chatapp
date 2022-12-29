4e052462d707eb6782027b7603da2338e1078bad
Bright Iortsor <iortsor@gmail.com>
2022-12-29 17:17:58 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-//import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 
 function App() {
+  if (!localStorage.getItem("username")) return <Login />;
+
   return (
-    <div className="App">
-      {/* <Login /> */}
-      <Signup />
-      {/* <Home /> */}
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="467ac5c7-a3d2-4a59-9c0a-fbd9f0aa2188"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,64 @@
 import React from "react";
 import "../Style.scss";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // get username and password from form
+    const authObject = {
+      "Project-ID": "467ac5c7-a3d2-4a59-9c0a-fbd9f0aa2188",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+    try {
+      // call to get messages
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+      // if it works, store username and password in local storage
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      // reload page after pessting login
+      window.location.reload();
+    } catch (error) {
+      // if it doesn't work, show error
+      setError("Oops, incorrect credentials.");
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="wrapper">
+    <div className="login-container">
+      <div className="login-wrapper">
         <span className="logo">iBiChat</span>
         <span className="title">Login</span>
-        <form>
-          <input type="text" placeholder="username" />
-          <input type="text" placeholder="password" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+            required
+          />
 
-          <button>Login</button>
+          <button type="submit">Login</button>
+          <h4 className="error">{error}</h4>
         </form>
-        <p>Don't have an account? Sign Up</p>
+        <p>Start chatting ...</p>
       </div>
     </div>
   );
